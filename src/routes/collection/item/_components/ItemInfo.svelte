99de78419f0e46982c51b1c7138703e5adb29e87
Jean-Baptiste Blanc <jb.blanc@consensys.net>
2022-06-29 10:06:00 +0200

    Spacing on item description

--- src/routes/collection/item/_components/ItemInfo.svelte
+++ src/routes/collection/item/_components/ItemInfo.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  import type { CollectionItem, Network } from "../../../../libs/interfaces";
-  import { getNetwork } from "../../../../libs/referential.js";
-  import ItemMetadata from "./ItemMetadata.svelte";
-  import { networks } from "../../../../stores/networks";
+  import type { CollectionItem, Network } from '../../../../libs/interfaces';
+  import { getNetwork } from '../../../../libs/referential.js';
+  import ItemMetadata from './ItemMetadata.svelte';
+  import { networks } from '../../../../stores/networks';
 
   export let item: CollectionItem;
   let network: Network;
@@ -13,11 +13,15 @@
 <div class="grid grid-flow">
   <h1 class="text-4xl font-bold">{item.attributes.title}</h1>
   <div>
-    <div class="badge border-blue-400 bg-blue-400 uppercase text-gray-200">{item.token_contract?.token_type}</div>
-    <div class="badge border-yellow-500 bg-yellow-500 uppercase text-gray-200">{network?.name}</div>
+    <div class="badge border-blue-400 bg-blue-400 uppercase text-gray-200">
+      {item.token_contract?.token_type}
+    </div>
+    <div class="badge border-yellow-500 bg-yellow-500 uppercase text-gray-200">
+      {network?.name}
+    </div>
     <div class="badge badge-outline uppercase">No active listing</div>
   </div>
-  <p>{item.attributes.description}</p>
+  <p class="mt-6">{item.attributes.description}</p>
   <div class="my-6">
     <ItemMetadata {item} />
   </div>
